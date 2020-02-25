8e1c86e12a050bd338e8b84249173700d6b5ddfc
Postmodern <postmodern.mod3@gmail.com>
2020-02-24 20:48:07 -0800

    The C definition of ioctl(2) is variadic.

--- src/sys/ioctl.cr
+++ src/sys/ioctl.cr
@@ -2,5 +2,5 @@ require "../bits/ioctls"
 require "../bits/ioctl-types"
 
 lib LibC
-  fun ioctl(fd : Int32, request : UInt32, output : Void *) : Int32
+  fun ioctl(fd : Int32, request : UInt32, ...) : Int32
 end
